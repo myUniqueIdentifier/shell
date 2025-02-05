b36c66567e628cf1cd42b3ceff05f7712c935bcb
Gerhard van Niekerk <gerhard.vanniekerk@za.saabgroup.com>
2025-02-05 17:07:21 +0200

    invalid command

--- src/main.cs
+++ src/main.cs
@@ -1,8 +1,10 @@
 using System.Net;
 using System.Net.Sockets;
 
-// Uncomment this line to pass the first stage
 Console.Write("$ ");
 
 // Wait for user input
 Console.ReadLine();
+
+Console.Write("invalid_command: command not found");
+Console.Write("$ ");
